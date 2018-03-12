7ebfacc196f6a14c89116018b71cf02196e3fd9a
mattNat <mattsanpedro76@gmail.com>
2018-03-11 22:29:01 -0700

    on /src/index.js add PostsIndex route path and wrapped Switch on routes

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
 // when are we going to call reaction creator
 // react lifecycle method
@@ -9,19 +10,46 @@ class PostsIndex extends Component {
   // one time loading procedure
   // ideal for data loading
   componentDidMount() {
+    // action creator, will concole.log twice
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    // this.props.posts is an object -> 
+    // cannot use array map method previously
+
+    // but... lodash's map function can deal with objects
+    return _.map(this.props.posts, post => {
+      return (
+        <li className='list-group-item' key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
+
   render() {
+    // will console log twice
+    // console.log(this.props.posts);
+    
     return (
       <div>
-        Posts Index
+        <h3>Post</h3>
+        <ul className='list-group'>
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
+// lists of post get inside component
+function mapsStateToProps(state) {
+  return { posts: state.posts };
+}
+
+
 // null - we are not passing mapsStateToProps
 // fetchPosts is identical to mapDispatchToProps
 // still have access to this.props.fetch.posts
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapsStateToProps, { fetchPosts })(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+// switch component takes in a collection of different routes
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 // past app was wrapper or main locations
 // with react-router, no single or central component going on...so don't need app component
 import reducers from './reducers';
 import PostsIndex from './components/post_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -16,8 +18,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        {/* there is a mistake in here */}
-        <Route path='/' component={PostsIndex} />
+        {/* Switch -> should load specific endpoints first */}
+        <Switch>
+          <Route path='/posts/new' component={PostsNew} />
+          <Route path='/' component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
