8c4dfeb8269c4c84d56ed94091bd452c9b15e455
mattNat <mattsanpedro76@gmail.com>
2018-03-11 20:59:12 -0700

    on /components/post_index.js add componentDidMount() and wire redux and fetchPosts action

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+// when are we going to call reaction creator
+// react lifecycle method
 
 class PostsIndex extends Component {
+  // one time loading procedure
+  // ideal for data loading
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +21,7 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+// null - we are not passing mapsStateToProps
+// fetchPosts is identical to mapDispatchToProps
+// still have access to this.props.fetch.posts
+export default connect(null, { fetchPosts })(PostsIndex);
