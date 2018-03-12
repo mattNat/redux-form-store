f136d1da8c525a5e05e20a21f83758feaf63ecb6
mattNat <mattsanpedro76@gmail.com>
2018-03-11 19:42:31 -0700

    add promise from redux-promise and add to applyMiddleware line 13 in /src/index.js

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export const FETCH_POSTS = 'FETCG_POSTS';
+
+
+export function fetchPosts() {
+  return {
+    type: FETCH_POSTS
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 // past app was wrapper or main locations
 // with react-router, no single or central component going on...so don't need app component
 import reducers from './reducers';
 import PostsIndex from './components/post_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
