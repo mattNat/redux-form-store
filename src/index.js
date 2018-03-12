c7bbb029350514d95d76cd711001616789c602e0
mattNat <mattsanpedro76@gmail.com>
2018-03-11 19:34:44 -0700

    add Routh path to /src/index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
+// past app was wrapper or main locations
+// with react-router, no single or central component going on...so don't need app component
 import reducers from './reducers';
+import PostsIndex from './components/post_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        {/* there is a mistake in here */}
+        <Route path='/' component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
