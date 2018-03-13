925a68230ebed8afd30ec3a4b66d4e135301ee55
mattNat <mattsanpedro76@gmail.com>
2018-03-13 13:30:03 -0700

    ready for generalizing fields

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 export const FETCH_POSTS = 'FETCH_POSTS';
 
 const API_KEY = '?key=matttheratatata';
-const ROOT_URL = `http://reduxblog.herokuapp.com/api`;
+const ROOT_URL = `http://localhost:8080/notes`;
 
 export function fetchPosts() {
-  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
+  // const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/notes${API_KEY}`);
+  
   return {
     type: FETCH_POSTS,
     payload: request
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
+  // field.input
+  /*
+  onChange={field.input.onChange}
+  onFocus={field.input.onFocus}
+  onBlur={field.input.onBlur}
+  {...field.input}
+  */
+  renderTitleField(field) {
     return (
       <div>
-        PostsNew!
+          <input
+            type='text'
+            {...field.input}
+          />
       </div>
-    );
+    )
+  }
+
+  render() {
+    return (
+      <form>
+        <Field
+          name='title'
+          component={this.renderTitleField}
+        />
+      </form>
+    )
   }
 }
 
-export default PostsNew;
+// wire up like connect helper
+export default reduxForm({
+  // name of the form
+  form: 'PostsNewForm'
+})(PostsNew);
