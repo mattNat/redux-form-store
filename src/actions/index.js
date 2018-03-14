f103e4d74bdc07aa36c88b5b157c4ad30a6f7099
mattNat <mattsanpedro76@gmail.com>
2018-03-13 22:35:47 -0700

    can post note to heroku

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,28 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const API_KEY = '?key=matttheratatata';
-const ROOT_URL = `http://localhost:8080/notes`;
+const ROOT_URL = `http://reduxblog.herokuapp.com/api`;
 
 export function fetchPosts() {
   // const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-  const request = axios.get(`${ROOT_URL}/notes${API_KEY}`);
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
   
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+// hook up to posts_new.js
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   // field.input
@@ -10,23 +13,50 @@ class PostsNew extends Component {
   {...field.input}
   */
   renderField(field) {
+    // es6 destructure
+    const { meta } = field;
+
+    const className = `form-group ${meta.touched && meta.error ? 'has-danger' : ''}`;
+
     return (
-      <div className='form-group'>
-          <label>{field.label}</label>
-          <input
-            className='form-control'
-            type='text'
-            {...field.input}
-          />
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className='form-control'
+          type='text'
+          {...field.input}
+        />
+        {/* add ternary operation to not display error at pristine condition */}
+        <div className='text-help'>
+          {meta.touched ? meta.error : ''}
+        </div>
       </div>
     )
   }
 
   // no paren, pass in ref to a function so field can re-render multiple times
   // "label" can be any property name
+  // name property communicates with validation errors below
+  // onSubmit needs reduc form and code we need to write
+  onSubmit(values) {
+    // this === component
+    // console.log(values);
+
+    // must match one of the routes defined in app
+    // only attempt nav only after post has been created
+    // callback function created, account for in reducer
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  };
+
   render() {
+    // passed to comp on behalf of redux form
+    const { handleSubmit } = this.props;
+
+    // redux (state and validation) cannot save/make post request
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label='Title'
           name='title'
@@ -42,6 +72,8 @@ class PostsNew extends Component {
           name='content'
           component={this.renderField}
         />
+        <button type='submit' className='btn btn-primary'>Submit</button>
+        <Link to='/' className='btn btn-danger'>Cancel</Link>
       </form>
     )
   }
@@ -52,17 +84,17 @@ function validate(values) {
   const errors = {};
 
   // validate the inputs from 'values'
-  if (values.title.length < 3) {
-    errors.title = 'Title must be at least 3 characters';
-  }
+  // if (values.title.length < 3) {
+  //   errors.title = 'Title must be at least 3 characters';
+  // }
   if (!values.title) {
-    error.title = 'Enter a title';
+    errors.title = 'Enter a title';
   }
   if (!values.categories) {
-    error.categories = 'Enter some categories';
+    errors.categories = 'Enter some categories';
   }
   if (!values.content) {
-    error.content = 'Enter some content please';
+    errors.content = 'Enter some content please';
   }
 
   // if errors is empty, the form is fine to submit
@@ -75,4 +107,6 @@ export default reduxForm({
   validate,
   // name of the form
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
