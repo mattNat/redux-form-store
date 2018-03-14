6b66af95691b0d615518383a7867ed2a1b802db0
mattNat <mattsanpedro76@gmail.com>
2018-03-13 23:42:40 -0700

    add loading to posts_show.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const FETCH_POST = 'FETCH_POST';
+
 
 const API_KEY = '?key=matttheratatata';
 const ROOT_URL = `http://reduxblog.herokuapp.com/api`;
@@ -25,4 +27,14 @@ export function createPost(values, callback) {
     type: CREATE_POST,
     payload: request
   };
+}
+
+// fetch a particular post
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  }
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    // provided by react-router
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  // helperFunction() {
+  //   this.props.posts[this.props.match.params.id];
+  // }
+
   render() {
+    // this.props === ownProps
+    // posts[this.props.match.params.id]; // post we want to show
+
+    // this.props.match.params.id;
+
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
     return (
       <div>
-        Posts Show!
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
-  };
+  }
 }
 
-export default PostsShow;
+// destructuring...just give me the list of posts
+function mapStateToProps({ posts }, ownProps) {
+  // return { posts };
+  return { post: posts[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
